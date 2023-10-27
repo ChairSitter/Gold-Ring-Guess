44eedfa3e23145607434e1fd8f5797c7f4ca651d
Bryan Schneller <bryan.schneller@gmail.com>
2023-10-27 12:53:44 -0400

    Change gold silver bronze to uppercase

--- match2.js
+++ match2.js
@@ -682,18 +682,18 @@ const countTotalValue = () => {
         winLose.innerHTML = `Total: ${differenceTotal}<br/>Rounds: ${roundCount}<br/>Difference: ${difference}<br/>Bonus: -${valueBonus}`;
     } else if(guessNumber == totalValue){
         differenceTotal = differenceTotal - (valueBonus * 2);
-        winLose.innerHTML = `Total: ${differenceTotal}<br/>Rounds: ${roundCount}<br/>PERFECT!<br/>Bonus x2 = -${valueBonus * 2}`;
+        winLose.innerHTML = `Total: ${differenceTotal}<br/>Rounds: ${roundCount}<br/>PERFECT!<br/>Bonus x2: -${valueBonus * 2}`;
     }
     if(roundCount === 10){
         if(differenceTotal < 1){
             winLose.style.color = "gold";
-            winLose.innerHTML = `${differenceTotal} points:<br/>Gold<br/>Medal!`
+            winLose.innerHTML = `${differenceTotal} points<br/>GOLD!`
         } else if(differenceTotal >= 1 && differenceTotal <= 50){
             winLose.style.color = "silver";
-            winLose.innerHTML = `${differenceTotal} points:<br/>Silver<br/>Medal!`
+            winLose.innerHTML = `${differenceTotal} points:<br/>SILVER!`
         } else if(differenceTotal > 50 && differenceTotal <= 100){
             winLose.style.color = "rgb(182, 114, 26)";
-            winLose.innerHTML = `${differenceTotal} points:<br/>Bronze<br/>Medal!`
+            winLose.innerHTML = `${differenceTotal} points<br/>BRONZE!`
         } else {
             winLose.innerHTML = `10-round<br/>score is<br/>${differenceTotal}!`
         }
